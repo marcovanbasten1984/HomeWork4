1d0502569c5bfddc44cee96ef8e283a9f3e87bf2
marcovanbasten1984 <clayton@ukr.net>
2021-08-10 00:00:34 +0300

    Initial Commit

---
+++ HomeWork4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWork4
+//
+//  Created by Admin on 10.08.21.
+//  Copyright © 2021 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
